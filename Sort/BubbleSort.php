218c5d5a2bef80cc2cca9ad9ff5514727149c1d6
bravesoft-kanehiroyuu <kanehiro@yuulinux.tokyo>
2021-01-04 23:57:19 +0900

    テストコード追加

--- Sort/BubbleSort.php
+++ Sort/BubbleSort.php
@@ -1,33 +1,53 @@
 <?php
 
-$data = [4, 6, 1, 2, 5, 3];
-$max = count($data);
+$data = range(0, 100);
+shuffle($data);
+$flag = false;
+main($flag, $data);
 
-// 要素数の繰り返し
-for ($i = 0; $i < $max; $i++) {
-    // 要素数-1回の繰り返し
-    for ($j = 1; $j < $max; $j++) {
-        // 隣同士を比較して順番に入れ替える
-        if ($data[$j-1] > $data[$j]) {
-            $temp = $data[$j];
-            $data[$j] = $data[$j-1];
-            $data[$j-1] = $temp;
-        }
+function main(bool $flag = true, array $data) {
+    switch ($flag) {
+        // テスト
+        case 0:
+            printf("テスト結果は「%s」です。", testBubbleSort());
+            break;
+        // 実行
+        case 1:
+            var_dump(bubbleSort($data));
+            break;
     }
 }
 
-var_dump($data);
-// array(6) {
-//     [0]=>
-//     int(1)
-//     [1]=>
-//     int(2)
-//     [2]=>
-//     int(3)
-//     [3]=>
-//     int(4)
-//     [4]=>
-//     int(5)
-//     [5]=>
-//     int(6)
-//   }
+
+function bubbleSort(array $data = []): array
+{
+    $max = count($data);
+
+    // 要素数の繰り返し
+    for ($i = 0; $i < $max; $i++) {
+        // 要素数-1回の繰り返し
+        for ($j = 1; $j < $max; $j++) {
+            // 隣同士を比較して順番に入れ替える
+            if ($data[$j-1] > $data[$j]) {
+                $temp = $data[$j];
+                $data[$j] = $data[$j-1];
+                $data[$j-1] = $temp;
+            }
+        }
+    }
+    return $data;
+}
+
+function testBubbleSort()
+{
+    for ($i = 0; $i < 100; $i++) {
+        $data = range(0, 1000);
+        shuffle($data);
+        $data1 = $data;
+        $data2 = $data;
+        if (bubbleSort($data1) != sort($data2)) {
+            return "不合格";
+        }
+    }
+    return "合格";
+}
