2733912af0fc907658b8796b335de4fd2b3c38a8
bravesoft-kanehiroyuu <kanehiro@yuulinux.tokyo>
2021-01-18 00:14:53 +0900

    ボゴソート

---
+++ Sort/BogoSort.php
@@ -0,0 +1,46 @@
+<?php
+
+$list = range(1, 8);
+shuffle($list);
+print_r(bogoSort($list));
+// Array
+// (
+//     [試行回数] => 7146
+//     [data] => Array
+//         (
+//             [0] => 1
+//             [1] => 2
+//             [2] => 3
+//             [3] => 4
+//             [4] => 5
+//             [5] => 6
+//             [6] => 7
+//             [7] => 8
+//         )
+// )
+
+
+/**
+ * ボゴソート
+ *
+ * @param array $list
+ */
+function bogoSort($list): array
+{
+    shuffle($list);
+
+    $clone_list = $list;
+    sort($clone_list);
+
+    $count = 1;
+    while (true) {
+        shuffle($list);
+        if ($clone_list == $list) {
+            return $response = [
+                '試行回数' => $count,
+                'data' => $list
+            ];
+        }
+        $count++;
+    }
+}
