450c203718423eca2f11564a22f99cb2c0e668a9
bravesoft-kanehiroyuu <kanehiro@yuulinux.tokyo>
2021-06-14 07:35:50 +0900

    D172:イヴの日付

---
+++ Paiza/172_getEveDay.php
@@ -0,0 +1,14 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $input_line = fgets(STDIN);
+    echo getEveDay($input_line);
+
+    function getEveDay(int $day): ?int
+    {
+        if ($day < 2) {
+            return null;
+        }
+        $eve_day = $day - 1;
+        return $eve_day;
+    }
